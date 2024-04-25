9514f2bea0ebabffd2b28f441b93262eca4bac8e
ramos2021110 <95529995+ramos2021110@users.noreply.github.com>
2024-04-25 08:45:58 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def index():
                            clientCrypto = Markup(public_key.decode("utf-8")))
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port = 3000, debug=True)
