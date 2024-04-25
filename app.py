759020aa6780f8ad8dbad1fbc18f6a75154895f6
ramos2021110 <95529995+ramos2021110@users.noreply.github.com>
2024-04-25 11:24:14 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import json
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
@@ -20,6 +21,9 @@ public_key = client_key.publickey().exportKey()
 
 app = Flask(__name__, template_folder="template")
 app.config['SECRET'] = "secret!123"
+
+# Read the PORT environment variable or default to port 3000
+port = int(os.environ.get('PORT', 3000))
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('message')
@@ -63,3 +67,6 @@ def index():
     return render_template('arbsshtml.html', 
                            clientCrypto = Markup(public_key.decode("utf-8")))
 
+if __name__ == '__main__':
+    # Run the Flask app with SocketIO support
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
