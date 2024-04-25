e4a55c8e58dcb0f9f52dbdf616a669de8b3efc37
ramos2021110 <95529995+ramos2021110@users.noreply.github.com>
2024-04-25 11:07:12 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import os
 import json
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, emit
 from markupsafe import Markup
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
@@ -15,9 +14,10 @@ key = RSA.generate(2048, random_generator)
 crypto_key = key.public_key().export_key()
 client_random_generator = Random.new().read
 private_random_generator = Random.new().read
-client_key = RSA.generate(2048, client_random_generator)
+client_key = RSA.generate(2048,client_random_generator)
 private_key = client_key.exportKey()
 public_key = client_key.publickey().exportKey()
+
 app = Flask(__name__, template_folder="template")
 app.config['SECRET'] = "secret!123"
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -25,43 +25,41 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 @socketio.on('message')
 def handle_message(message):
     client_key_pv = RSA.importKey(private_key)
-    client_key_decrypter = PKCS1_OAEP.new(client_key_pv, hashAlgo=SHA256)
     
-    try:
-        json_dumped = json.dumps(message)
-        json_message = json.loads(json_dumped)
-
-        if json_message.get("twoparts", False):
-            ciphertext1 = b64decode(json_message.get("userMessageP1", ""))
-            decrypted1 = client_key_decrypter.decrypt(ciphertext1)
-            decoded1 = decrypted1.decode('UTF-8')
-
-            ciphertext2 = b64decode(json_message.get("userMessageP2", ""))
-            decrypted2 = client_key_decrypter.decrypt(ciphertext2)
-            decoded2 = decrypted2.decode('UTF-8')
-
-            full_message = decoded1 + decoded2
-            response = get_chat_response(full_message)
-        else:
-            ciphertext = b64decode(json_message.get("userMessage", ""))
-            decrypted = client_key_decrypter.decrypt(ciphertext)
-            full_message = decrypted.decode('UTF-8')
-            response = get_chat_response(full_message)
-
-        emit("arby_answers", {"message": response})
-
-    except Exception as e:
-        print("Decryption error:", e)
-        # Handle the decryption error gracefully
+    client_key_decrypter = PKCS1_OAEP.new(client_key_pv, hashAlgo=SHA256)
+    json_dumped = json.dumps(message)
+    json_message = json.loads(json_dumped)
+    global botMessage
+    if (json_message["twoparts"]):
+        print(json_message["userMessageP1"])
+        ciphertext1 = b64decode(json_message["userMessageP1"])
+        decrypted1 = client_key_decrypter.decrypt(ciphertext1)
+        decoded1 = decrypted1.decode('UTF-8')
+        print(decoded1)
+        ciphertext2 = b64decode(json_message["userMessageP2"])
+        decrypted2 = client_key_decrypter.decrypt(ciphertext2)
+        decoded2 = decrypted2.decode('UTF-8')
+        print(decoded2)
+        full_message = decoded1+''+decoded2
+        print(full_message)
+        response = get_chat_response(full_message)
+        botMessage = response
+        emit("arby_answers", {"message": botMessage})
+    else:
+        print(json_message["userMessage"])
+        ciphertext = b64decode(json_message["userMessage"])
+        decrypted = client_key_decrypter.decrypt(ciphertext)
+        decoded = decrypted.decode('UTF-8')
+        print(decoded)
+        response = get_chat_response(decoded)
+        botMessage = response
+        emit("arby_answers", {"message": botMessage})
 
 @app.get("/")
 def index():
-    print(public_key)
-    print(private_key)
-    print('Sending...')
+    print (public_key)
+    print (private_key)
+    print ('Sending...')
     return render_template('arbsshtml.html', 
-                           clientCrypto=Markup(public_key.decode("utf-8")))
+                           clientCrypto = Markup(public_key.decode("utf-8")))
 
-if __name__ == '__main__':
-    # Run the Flask app with SocketIO support
-    socketio.run(app, host="0.0.0.0", debug=True)
